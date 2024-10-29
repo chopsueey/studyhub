00dc7d064739ec61a135b665319bfb2912137f2d
chopsueey <meyer-marius@live.de>
2024-10-29 15:16:55 +0100

    add quill and react-quilljs for a editor component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+'use client'
+
+import QuillEditor from './components/quill-editor';
+
 export default function Home() {
+
   return (
-   <div>Hello world!</div>
+    <div>
+      <QuillEditor />
+      
+    </div>
   );
 }

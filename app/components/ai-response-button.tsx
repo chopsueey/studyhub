a356e275f80bdfe827a5bd87238ee68387c0a76a
chopsueey <meyer-marius@live.de>
2024-11-09 12:45:52 +0100

    change the details elements behaviour: opening and closing independently, style changes

--- app/components/ai-response-button.tsx
+++ app/components/ai-response-button.tsx
@@ -12,7 +12,7 @@ export default function AiResponseButton({
 }) {
   const [response, setResponse] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const options = ["Summary", "Keywords", "Quiz"];
+  const options = ["Summary", "Terms and keywords", "Quiz"];
 
   const handleGenerateResponse = async () => {
     const res = await fetch("/api/generateAIResponse", {
@@ -32,29 +32,23 @@ export default function AiResponseButton({
 
   return (
     <div className="max-w-screen-sm">
-      <button
-        className="w-fit px-4 py-2 rounded-lg bg-green-500 text-white font-semibold shadow-sm hover:bg-green-600 hover:shadow-md transition-all duration-300"
-        onClick={handleGenerateResponse}
-      >
-        Create {options[option]}
-      </button>
-      {isOpen && (
-        <details open={isOpen} className="group border my-4 border-gray-200 rounded-lg shadow-sm transition-all duration-300 open:shadow-lg open:border-gray-300">
-          <summary className="flex items-center justify-between cursor-pointer p-4 text-lg font-semibold text-gray-700 bg-gray-100 rounded-lg group-open:bg-gray-200 group-open:text-gray-800 transition-all duration-300">
-            More Information
-            <span className="ml-2 transform transition-transform duration-300 group-open:rotate-180">
-              ▼
-            </span>
-          </summary>
-          <div className="p-4 text-gray-600 bg-gray-50 rounded-b-lg overflow-y-scroll max-h-[50vh]">
-            <ReactMarkDown>{response}</ReactMarkDown>
-          </div>
-        </details>
-      )}
-
-      {/* <details open={isOpen}>
-        <ReactMarkDown>{response}</ReactMarkDown>
-      </details> */}
+      <details className="group w-fit border my-4 border-gray-200 rounded-lg shadow-sm transition-all duration-300 open:shadow-lg open:border-gray-300">
+        <summary className="flex items-center justify-between cursor-pointer p-4 text-lg font-semibold text-gray-700 bg-gray-100 rounded-lg group-open:bg-gray-200 group-open:text-gray-800 transition-all duration-300">
+          {options[option]}
+          <span className="ml-2 transform transition-transform duration-300 group-open:rotate-180">
+            ▼
+          </span>
+        </summary>
+        <div className="flex flex-col p-4 text-gray-600 bg-gray-50 rounded-b-lg overflow-y-scroll max-h-[50vh]">
+          <ReactMarkDown>{response}</ReactMarkDown>
+          <button
+            className="w-fit px-4 py-2 ml-auto mt-4 rounded-lg bg-green-500 text-white font-semibold shadow-sm hover:bg-green-600 hover:shadow-md transition-all duration-300"
+            onClick={handleGenerateResponse}
+          >
+            {isOpen ? "Create again" : `Create`}
+          </button>
+        </div>
+      </details>
     </div>
   );
 }

dba3f2726f4349408984943407470b5eb8e7ec72
chopsueey <meyer-marius@live.de>
2024-11-25 18:56:09 +0100

    close sidebar on outside click

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,13 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AiResponseButton from "./ai-response-button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Sidebar({ noteId }: { noteId: string }) {
   const [isOpen, setIsOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+  function handleOutsideClick(e: MouseEvent) {
+    const sidebar = sidebarRef.current;
+
+    if (sidebar && !sidebar.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isOpen]);
   return (
     <div
+    ref={sidebarRef}
       className={`h-screen w-1/2 fixed top-0 left-0 flex flex-col space-y-8 overflow-y-auto p-8 bg-white border rounded-2xl shadow-md transition-all duration-300 ${
         isOpen ? "-translate-x-[0]" : "-translate-x-[90%]"
       }`}
